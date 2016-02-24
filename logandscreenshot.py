d0b80b3e41119a2c3eeac61e9a8a0ef9fb75a85e
kirktrekkie <kirk@mail2trekkie.com>
2016-02-24 13:18:45 +0100

    Changed all log to use logging, and removed some code

--- logandscreenshot.py
+++ logandscreenshot.py
@@ -33,10 +33,7 @@ class LogAndScreenshot():
         self.iterationstotal = 60 * self.testminutes / 3
         self.processfilter = processfilter #['firefox', 'python', 'pycharm', 'EXCEL', 'taskmgr', 'explorer', 'cmd']
         logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-
-    def log(self, message):
-        if self.logenabled:
-            logging.debug(message)
+        logging.disable(logging.DEBUG)
 
     def process_args(self):
         sys.argv.pop(0)
@@ -50,7 +47,7 @@ class LogAndScreenshot():
                 self.set_parameters(line)
             f.close()
         except FileNotFoundError as e:
-            self.log("Settings file not created. %s"%e)
+            logging.warning("Settings file not found. %s"%e)
 
     def set_parameters(self,parameter):
         # Parse parameters from command args or settings.txt
@@ -71,14 +68,14 @@ class LogAndScreenshot():
         elif temp[0] == 'imageformat':
             self.imageformat = temp[1]
         else:
-            self.log("Unknown parameter: %s" %(temp[0]))
+            logging.warning("Unknown parameter: %s" %(temp[0]))
             return "Unknown parameter: %s" %(temp[0])
 
     def iterations(self):
         # Calculate number of iterations to run
         if self.testminutes != 60 * 24:
             self.iterationstotal = self.testminutes*60 / 3
-        self.log("iterationstotal: %d" %self.iterationstotal)
+        logging.info("iterationstotal: %d" %self.iterationstotal)
         return int(self.iterationstotal)
 
     def file_name_and_path(self):
@@ -88,7 +85,7 @@ class LogAndScreenshot():
         if not exists(self.filepathname):
             mkdir(self.filepathname)
         self.filepathname += "\\" + name
-        self.log("totalpath: " + self.filepathname)
+        logging.debug("totalpath: " + self.filepathname)
         return self.filepathname
 
     def screenshot(self):
@@ -115,14 +112,12 @@ class LogAndScreenshot():
                     if pfilter in proc.name():
                         result.append(pool.apply_async(self.take_out_computer_info, (proc,)))
                         break
-                        #self.log(clock())
 
         # While threads are running take out total cpu_percent and start write to file
         f = open(logfile, 'w+')
         f.write('Total cpu_percent: %f' % (psutil.cpu_percent(1)) + '\n')
         f.write('Total memory stats: %s' % (str(psutil.virtual_memory())) + '\n\n')
         f.write(LOG_FILE_HEADER)
-        #sleep(1)
 
         # Get the results from the threads and write them to file
         for res in result:
@@ -133,7 +128,7 @@ class LogAndScreenshot():
             except (ProcessLookupError, psutil.NoSuchProcess) as e:
                 self.log("Process not found. %s" %e)
             f.write('\n')
-        self.log("Close file")
+        logging.debug("Close file")
         pool.close()
         pool.terminate()
         pool.join()
@@ -152,7 +147,7 @@ class LogAndScreenshot():
             ]
             return pinfo
         except ProcessLookupError as e:
-            self.log("Process not found. %s" %e)
+            logging.info("Process not found. %s" %e)
 
     def file_writer(self,file,info):
         if type(info) != 'str':
@@ -165,12 +160,12 @@ if __name__ == '__main__':
     logger.read_settings_file()
     logger.process_args()
     for i in range(logger.iterations()):
-        logger.log("iteration: %d"%i)
+        logging.info("iteration: %d"%i)
         start = clock()
         logger.file_name_and_path()
         logger.screenshot()
         logger.computerinfo()
         end = clock()
-        logger.log("end-start: %s" %(end-start))
+        logging.debug("end-start: %s" %(end-start))
         if end-start < 3:
             sleep(3-(end-start))
